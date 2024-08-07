69d23d1ef746e9c56b71d733d8fb4ba5e16d0cda
K <2144494@cognizant.com>
2024-08-07 22:20:18 +0530

    updated module

--- src/app.py
+++ src/app.py
@@ -1,45 +1,37 @@
+# import google.generativeai as genai
+import uvicorn
+import dotenv
+import os
+ 
 from fastapi import FastAPI
+from routes import model_comparision
 from routes import validate
 from routes import benchmarks_tg_svc
 from routes import benchmarks_qa_svc
-import uvicorn
-
-import os
-# import dotenv
-
+ 
 # Load environment variables from .env file
-# dotenv.load_dotenv()
-
+dotenv.load_dotenv()
+ 
 # Log in to Hugging Face Hub
 from huggingface_hub import login
-print(os.environ.get("HUGGINGFACE_TOKEN"))
 login(token=os.environ.get("HUGGINGFACE_TOKEN"))
-
+#print(os.environ.get("HUGGINGFACE_TOKEN"))
+ 
+# GOOGLE_GEMENI_API = os.environ.get('GOOGLE_GEMENI_API_KEY')
+# genai.configure(api_key=GOOGLE_GEMENI_API)
+ 
+ 
 app = FastAPI()
-
-# Include routers
+ 
+# # Include routers
+app.include_router(model_comparision.router)
 app.include_router(validate.router)
 app.include_router(benchmarks_tg_svc.router)
 app.include_router(benchmarks_qa_svc.router)
-
-
+ 
+ 
 if __name__ == "__main__":
     # Run the FastAPI app
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-
-
-
-
-
-# from services.benchmarks_tg import *
-
-# if __name__ == "__main__":
-#     test=LMEvalRunner()
-#     model_args= input("Enter the model name or path: ")
-#     test.get_lm_eval_command(model_args)
-#     test.clean_memory()
-
-
-
-
+ 
+ 
--- src/routes/validate.py
+++ src/routes/validate.py
@@ -1,56 +1,68 @@
 from fastapi import FastAPI, UploadFile ,File, HTTPException, status, APIRouter, UploadFile, Response, Form
 from fastapi.responses import FileResponse
- 
+from services.metrics_flant5 import Evaluator_flanT5
+
 from utils.data_models import *
-from services.metrics import Evaluator
+from services.metrics_gpt import Evaluator_gpt
+from services.metrics_flant5 import Evaluator_flanT5
 from services.azure_services import *
+from utils.function import *
 from utils.constants import *
- 
+
 import pandas as pd
 import io
 import os
 import json
 import shutil
- 
- 
-router = APIRouter(prefix='/val/metrics',tags=['metrics'])
- 
-evaluator = Evaluator()
- 
-@router.post("/upload_csv")
-async def upload_csv(file: UploadFile= File(...)):
- 
+
+
+router = APIRouter(prefix='/val/metrics',tags=['metrics']) 
+logger = Initialize_logger()
+
+evaluator_gpt = Evaluator_gpt()
+evaluator_flant5 = Evaluator_flanT5()
+
+
+@router.post("/upload_csv_gpt")
+async def upload_csv_gpt(file: UploadFile= File(...)):
+
     """
     Endpoint to receive an input file and generate metrics.
     It accepts a save path and a csv file, generates metrics, and returns the resulting JSON file.
- 
+
     Args:
     - file (UploadFile): The input file data for which metrics are to be generated.
- 
+
     Returns:
     - FileResponse: A response containing the generated JSON file of metrics.
     """
- 
+
     try:
- 
+
         az = azure_ops(account_name = os.environ.get('ACCOUNT_NAME'),
                         account_key = os.environ.get('ACCOUNT_KEY'),
                         container_name = os.environ.get('CONTAINER_NAME'),
                         blob_path=os.environ.get('VALIDATION')
                         )
-       
+        
         if not file:
+            logger.error("No file uploaded")
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No file uploaded")
- 
+            
+
+
         # Ensure the temporary save path exists
         os.makedirs(TMP_SAVE_UPLOAD_FILE_PATH, exist_ok=True)
- 
+        logger.info("TMP_SAVE_UPLOAD_FILE_PATH created successfully")
+
+
         # Validate the file extension
         if file.filename.endswith('.csv'):
                 print("Entered the file")
                 # Read the file contents
                 contents = await file.read()
- 
+                logger.info("Loading file data")
+
                 # Process the contents (convert to DataFrame)
                 df = pd.read_csv(io.BytesIO(contents))
                 len_=len(df)
@@ -58,62 +70,248 @@ async def upload_csv(file: UploadFile= File(...)):
                 list_of_metrics=[]
                 for i in range(0,len_):
                     row=df.iloc[i].values
-                    question= row[0]
-                    answer= row[1]
+                    # question= row[0]
+                    # answer= row[1]
+                    # reference_answer= row[2]
+                    # result_dict= evaluator_gpt.evaluate_all(question, answer, reference_answer)
+                    
+                    context= row[0]
+                    question= row[1]
                     reference_answer= row[2]
-                    result_dict= evaluator.evaluate_all(question, answer, reference_answer)
-                    list_of_metrics.append(result_dict)
-                    list_of_metrics_result.append({"result "+str(i+1):result_dict})
-                    print(i+1,result_dict)
-                   
-                   
-                metrics= evaluator.evaluate_average(list_of_metrics)
+                    logger.info(f"Row {i},inserted")
+
+                    gen_answer_gpt = evaluator_gpt.generate_answer(context, question)
+                    print(f"Answer: {gen_answer_gpt}")
+                    result_gpt= evaluator_gpt.evaluate_all(gen_answer_gpt, reference_answer)
+                    
+                    list_of_metrics.append(result_gpt)
+                    list_of_metrics_result.append({"result "+str(i+1):result_gpt})
+                                      
+                    
+                metrics= evaluator_gpt.evaluate_average(list_of_metrics)
                 list_of_metrics_result.append(metrics)
- 
-                print(metrics)
- 
+                logger.info("Metrics generated for GPT model")
+
                 # Define the path for the resulting JSON file
                 json_file_path = os.path.join(TMP_SAVE_UPLOAD_FILE_PATH, f"{file.filename.split('.')[0]}_metric_result.json")
-               
+                
                 # Save the embeddings to the specified path
                 with open(json_file_path, 'w') as json_file:
                     json.dump(list_of_metrics_result, json_file,indent=3 )
-                   
+                    
+                logger.info("JSON file created for GPT model")
                 print("json file created")
- 
+
                 sas_url = az.upload_file(local_file_path=json_file_path, file_name=f"{file.filename.split('.')[0]}_metrics.json")
                 shutil.rmtree(TMP_SAVE_UPLOAD_FILE_PATH)
+                logger.info("Result file uploaded to Azure")
                 return {
                     "message": "successful",
                     "metric_file": sas_url
                 }
- 
-           
+                
+
+            
         else:
+            logger.error("Not provided a valid file")
             raise HTTPException(status_code=422, detail="Please provide a valid CSV file.")
- 
- 
- 
+
+
+
     except HTTPException as http_exc:
         # Catch HTTP exceptions and re-raise them
+        logger.error(f"exception:{http_exc}")
         raise http_exc
+
     except Exception as e:
         # Catch any other exceptions and return a 500 server error
+        logger.error(f"Request not processed successfully. Details:{e}")
         raise HTTPException(status_code=500, detail=str(e))
         print(e)
-   
- 
- 
- 
-@router.post('/model_validation')
-def model_validation(payload: model_validation_input_shcema_text):
+
+    
+
+
+
+@router.post("/upload_csv_flant5")
+async def upload_csv_flant5(file: UploadFile= File(...)):
+
+    """
+    Endpoint to receive an input file and generate metrics.
+    It accepts a save path and a csv file, generates metrics, and returns the resulting JSON file.
+
+    Args:
+    - file (UploadFile): The input file data for which metrics are to be generated.
+
+    Returns:
+    - FileResponse: A response containing the generated JSON file of metrics.
+    """
+
     try:
-       
+
+        az = azure_ops(account_name = os.environ.get('ACCOUNT_NAME'),
+                        account_key = os.environ.get('ACCOUNT_KEY'),
+                        container_name = os.environ.get('CONTAINER_NAME'),
+                        blob_path=os.environ.get('VALIDATION')
+                        )
+        
+        if not file:
+            logger.error("No file uploaded")
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="No file uploaded")
+
+        # Ensure the temporary save path exists
+        os.makedirs(TMP_SAVE_UPLOAD_FILE_PATH, exist_ok=True)
+        logger.info("TMP_SAVE_UPLOAD_FILE_PATH created successfully")
+
+        # Validate the file extension
+        if file.filename.endswith('.csv'):
+                print("Entered the file")
+                # Read the file contents
+                contents = await file.read()
+                logger.info("Loading file data")
+
+                # Process the contents (convert to DataFrame)
+                df = pd.read_csv(io.BytesIO(contents))
+                len_=len(df)
+                list_of_metrics_result=[]
+                list_of_metrics=[]
+                for i in range(0,len_):
+                    row=df.iloc[i].values
+                    # question= row[0]
+                    # answer= row[1]
+                    # reference_answer= row[2]
+                    # result_dict= evaluator_gpt.evaluate_all(question, answer, reference_answer)
+
+                    context= row[0]
+                    question= row[1]
+                    reference_answer= row[2]
+                    logger.info(f"Row {i},inserted")
+
+                    gen_answer_flant5 = evaluator_flant5.generate_answer(context, question)
+                    print(f"Answer: {gen_answer_flant5}")
+                    result_flant5= evaluator_flant5.evaluate_all(gen_answer_flant5, reference_answer)
+                    
+                    list_of_metrics.append(result_flant5)
+                    list_of_metrics_result.append({"result "+str(i+1):result_flant5})
+                    #print(i+1,result_dict)
+                    
+                    
+                metrics= evaluator_flant5.evaluate_average(list_of_metrics)
+                list_of_metrics_result.append(metrics)
+                logger.info("Metrics generated for FlanT5 model")
+
+
+                # Define the path for the resulting JSON file
+                json_file_path = os.path.join(TMP_SAVE_UPLOAD_FILE_PATH, f"{file.filename.split('.')[0]}_metric_result.json")
+                
+                # Save the embeddings to the specified path
+                with open(json_file_path, 'w') as json_file:
+                    json.dump(list_of_metrics_result, json_file,indent=3 )
+                    
+                logger.info("JSON file created for flanT5 model")
+                print("json file created")
+
+                sas_url = az.upload_file(local_file_path=json_file_path, file_name=f"{file.filename.split('.')[0]}_metrics.json")
+                shutil.rmtree(TMP_SAVE_UPLOAD_FILE_PATH)
+                logger.info("Result file uploaded to Azure")
+                return {
+                    "message": "successful",
+                    "metric_file": sas_url
+                }
+                
+
+            
+        else:
+            logger.error("Not provided a valid file")
+            raise HTTPException(status_code=422, detail="Please provide a valid CSV file.")
+
+
+
+    except HTTPException as http_exc:
+        # Catch HTTP exceptions and re-raise them
+        logger.error(f"exception:{http_exc}")
+        raise http_exc
+
+    except Exception as e:
+        # Catch any other exceptions and return a 500 server error
+        logger.error(f"Request not processed successfully. Details:{e}")
+        raise HTTPException(status_code=500, detail=str(e))
+        print(e)
+
+
+
+
+
+@router.post('/model_validation_gpt')
+def model_validation_gpt(payload: model_validation_input_shcema_text):
+    try:
+        
         payload = payload.dict()
-        metrics = evaluator.evaluate_all(payload['question'], payload['answer'], payload['reference_answer'])
+        logger.info(f"Payload: {payload}")
+        if not payload:
+            logger.error("Payload not found")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="payload not found")
+
+        gen_answer_gpt = evaluator_gpt.generate_answer(payload['context'], payload['question'])
+        #print(f"Answer: {gen_answer_gpt}")
+
+        if not gen_answer_gpt:
+            logger.error("Generated response not found")
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Generated response not found")
+        logger.info(f"Answer: {gen_answer_gpt}")
+
+        metrics_gpt = evaluator_gpt.evaluate_all( gen_answer_gpt, payload['reference_answer'])
+        logger.info("Metrics computed successfully for GPT")
         return {
             "message": "successful",
-            "metrics": metrics
+            "metrics": metrics_gpt
         }
     except Exception as e:
+        logger.error(f"Exception in generated response:{e}")
+        print(traceback.format_exc())
+
+
+
+
+@router.post('/model_validation_flant5')
+def model_validation_flant5(payload: model_validation_input_shcema_text):
+    try:
+        payload = payload.dict()
+        print("Entered API")
+        print("data are as follows:")
+        print("context",payload['context'])
+        print("question",payload['question'])
+        print("reference_answer",payload['reference_answer'])
+
+        logger.info(f"Payload: {payload}")
+        if not payload:
+            logger.error("Payload not found")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="payload not found")
+
+        gen_answer_flant5 = evaluator_flant5.generate_answer(payload['context'], payload['question'])
+        if not gen_answer_flant5:
+            logger.error("Generated response not found")
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Generated response not found")
+        logger.info(f"Answer: {gen_answer_flant5}")
+        print(f"Answer: {gen_answer_flant5}")
+
+        metrics_flant5 = evaluator_flant5.evaluate_all( gen_answer_flant5, payload['reference_answer'])
+        logger.info("Metrics computed successfully for FlanT5")
+        print("Flant5 metrics computed")
+        return {
+            "message": "successful",
+            "metrics": metrics_flant5
+        }
+    except Exception as e:
+        logger.error(f"Exception in generated response:{e}")
         print("traceback.format_exc()")
+       
+        
+    
+
+        
+
+    
+        
+
+
